b17e1fef91a1028c57f6b02504f76dc4e2108ce7
MCocaFilho <marcoscocafilho@gmail.com>
2022-09-03 11:56:05 -0300

    Separa rotas por contexto e adiciona middleware de log e erro

---
+++ src/middlewares/logger.js
@@ -0,0 +1,11 @@
+const logRequest = (log = console.log) => (req, _res, next) => {
+    log({
+        url: req.url,
+        method: req.method,
+        headers: req.headers,
+        body: req.body
+    })
+    next()
+}
+
+module.exports = logRequest
